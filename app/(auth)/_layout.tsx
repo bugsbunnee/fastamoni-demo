63d45943a61f3e854ad1a9b9bddb7b9b83703bdb
Chukwuma Marcel Nsidinanya <chukwumamarcelnsidinanya@Chukwumas-MacBook-Air.local>
2023-11-07 18:01:34 +0100

    final-touches

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,31 +1,27 @@
+import React from 'react';
 import { Stack } from 'expo-router';
+
 import Text from '@/components/themed/Text';
 
 import useColor from '@/hooks/useColor';
 
-const AuthLayout = () => {
+const AuthLayout: React.FC = () => {
 	const color = useColor();
+
+	const visibleHeaderOptions = {
+		headerTintColor: color.text,
+		headerBackTitleVisible: false,
+		headerTitle: (props: { children: string }) => (
+			<Text style={{ textTransform: 'capitalize' }}>{props.children}</Text>
+		),
+	};
 
 	return (
 		<Stack>
 			<Stack.Screen name="theme" options={{ headerShown: false }} />
 			<Stack.Screen name="welcome" options={{ headerShown: false }} />
-			<Stack.Screen
-				name="login"
-				options={{
-					headerTitle: () => <Text>Login</Text>,
-					headerTintColor: color.text,
-					headerBackTitleVisible: false,
-				}}
-			/>
-			<Stack.Screen
-				name="register"
-				options={{
-					headerTitle: () => <Text>Register</Text>,
-					headerTintColor: color.text,
-					headerBackTitleVisible: false,
-				}}
-			/>
+			<Stack.Screen name="login" options={visibleHeaderOptions} />
+			<Stack.Screen name="register" options={visibleHeaderOptions} />
 		</Stack>
 	);
 };
